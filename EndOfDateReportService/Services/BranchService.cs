74a74c5e9ba3fb2bb71b9e72c3c0378fa02a67d4
timoteo0204 <timoteo0204@gmail.com>
2023-10-11 23:31:24 -0300

    services

--- EndOfDateReportService/Services/BranchService.cs
+++ EndOfDateReportService/Services/BranchService.cs
@@ -19,16 +19,16 @@ namespace EndOfDateReportService.Services
         private Repository _repository;
         private readonly IConfiguration _configuration;
         private PdfService _pdfService;
-        //private ExcelService _excelService;
+        private ExcelService _excelService;
         private readonly IMapper _mapper;
-        public BranchService(IConfiguration configuration, ReportContext reportContext, Repository repository, PdfService pdfService, /*ExcelService excelService,*/ IMapper mapper)
+        public BranchService(IConfiguration configuration, ReportContext reportContext, Repository repository, PdfService pdfService, ExcelService excelService, IMapper mapper)
         {
             _configuration = configuration;
             connectionString = configuration.GetConnectionString("DefaultConnection");
             _reportContext = reportContext;
             _repository = repository;
             _pdfService = pdfService;
-            //_excelService = excelService;
+            _excelService = excelService;
             _mapper = mapper;
         }
 
@@ -149,10 +149,10 @@ namespace EndOfDateReportService.Services
             return null;
         }
 
-        //public async Task ExcelGenerator(DateTime fromDate, DateTime toDate)
-        //{
-        //    await _excelService.ExportToExcel(fromDate, toDate);
-        //}
+        public async Task ExcelGenerator(DateTime fromDate, DateTime toDate)
+        {
+            await _excelService.ExportToExcel(fromDate, toDate);
+        }
 
         public async Task<List<BranchModelOut>> AddFees(IEnumerable<Branch> branches, DateTime date)
         {
