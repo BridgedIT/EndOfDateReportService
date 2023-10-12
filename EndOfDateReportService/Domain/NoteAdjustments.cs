f1acb1c71b2647ec843bc897f4622e069d8d61eb
jrabunal <juanmarabunal@gmail.com>
2023-10-12 00:20:01 -0300

    Note adjustments class

---
+++ EndOfDateReportService/Domain/NoteAdjustments.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EndOfDateReportService.Domain
+{
+    public class NoteAdjustments
+    {
+        [Key]
+        public int BranchId { get; set; }
+        [Key]
+        public int LaneId { get; set; }
+        public DateTime Date { get; set; }  
+        public string? Comments { get; set; }
+        public double? CallAdjustments { get; set; }
+    }
+}
