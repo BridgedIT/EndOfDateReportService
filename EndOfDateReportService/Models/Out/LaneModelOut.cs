2a5ab8485487a72bb76df2a93e3e68f5088d6fdf
timoteo0204 <timoteo0204@gmail.com>
2023-10-23 23:14:46 -0300

    fix

--- EndOfDateReportService/Models/Out/LaneModelOut.cs
+++ EndOfDateReportService/Models/Out/LaneModelOut.cs
@@ -4,5 +4,7 @@ public class LaneModelOut
 {
     public int LaneId { get; set; }
     public int BranchId { get; set; }
+    public string? Note { get; set; }
+    public double? Adjustment { get; set; }
     public ICollection<PaymentMethodModelOut> PaymentMethods { get; set; }
 }
